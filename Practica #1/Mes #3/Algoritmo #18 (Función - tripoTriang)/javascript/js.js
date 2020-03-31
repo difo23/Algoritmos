a07b4341a2d489a6054f54971a4232566046101f
Lizandro <lizandro.ramirez@ucateci.edu.do>
2020-03-31 17:43:13 -0400

    Condicion isoceles
    La condición isósceles tiene problemas, debes verificar que sea isósceles si tiene por lo menos dos lados iguales.

--- Practica #1/Mes #3/Algoritmo #18 (Función - tripoTriang)/javascript/js.js
+++ Practica #1/Mes #3/Algoritmo #18 (Función - tripoTriang)/javascript/js.js
@@ -8,6 +8,8 @@ var a = parseInt (prompt ("Longitud del lado (A)."));
     if (a==b && b==c){
         document.write("<h1>Es<u> Equilatero</u></h1>")
     }else if(a!=b && b!=c){
+        //Ojo Con esta condicion, estas preguntando si son diferentes. 
+        //El Isoceles tiene por lo menos dos lados iguales.
         document.write("<h1>Es<u> Isoceles</u></h1>")
 
     }else {
